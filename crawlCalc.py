aee946693ec5e91b07f04b91e65261ce051065e9
Salanmander <Alan.Kraut@gmail.com>
2020-05-24 18:57:24 -0700

    Added histogram readout

--- crawlCalc.py
+++ crawlCalc.py
@@ -3,49 +3,138 @@ import statistics
 
 def main():
     num_trials = 10000
+    print_histograms = True
+    histogram_bins = 10
+
+    weapons = []
     
-    weap1_base = 12
-    weap1_slay = 7
-    weap1_st_bonus = 0
-    weap1_brand = "electrocution"
+    weapons.append({'name': 'trishula',
+                    'base': 13,
+                    'slay': 9,
+                    'st_bonus': 0,
+                    'brand': 'none',
+                    'skill': 17.9})
 
-    weap2_base = 13
-    weap2_slay = 3
-    weap2_st_bonus = 0
-    weap2_brand = "holy wrath"
+    
+    weapons.append({'name': 'demon trident',
+                    'base': 12,
+                    'slay': 9,
+                    'st_bonus': 0,
+                    'brand': 'electrocution',
+                    'skill': 17.9})
+    """
 
-    st = 27
-    wp_skill = 14
-    ft_skill = 11.9
+    weapons.append({'name': 'antimagic spear',
+                    'base': 6,
+                    'slay': 5,
+                    'st_bonus': 4,
+                    'brand': 'none',
+                    'skill': 27})
+    
+    weapons.append({'name': 'antimagic trident',
+                    'base': 9,
+                    'slay': 0,
+                    'st_bonus': 0,
+                    'brand': 'none',
+                    'skill': 27})
+    """
+
+    st = 31
+    dex = 10
+    ft_skill = 23.5
     extra_slay = 3
 
     enemy_ac = 0
-    
-    dmg_list = []
-    for _ in range(num_trials):
-        dmg =  final_damage(weap1_base, st + weap1_st_bonus,
-                            wp_skill, ft_skill,
-                            weap1_slay, enemy_ac,
-                            weap1_brand)
-        dmg_list.append(dmg)
+
+    for weap in weapons:
+        dmg_list = []
+        for _ in range(num_trials):
+            dmg =  final_damage(weap['base'], st + weap['st_bonus'],
+                                weap['skill'], ft_skill,
+                                weap['slay'] + extra_slay, enemy_ac,
+                                weap['brand'])
+            dmg_list.append(dmg)
+            
+        dmg1 = statistics.mean(dmg_list)
+
+        print("\n\n" + weap['name'] + ": " + str(dmg1) + "\n")
         
+        if print_histograms:
+            print_histogram(dmg_list, histogram_bins)
 
-    dmg1 = statistics.mean(dmg_list)
 
+# Prints the percentage of elements in the list that fall into each bin.
+# Bin sizes are uniform, except for the bin of exactly-zero.
+# Total number of bins is given by num_bins, so size of non-zero bins is
+# max_val/(num_bins - 1)
+def print_histogram(values, num_bins):
+    values.sort()
+    num_vals = len(values)
+
+    max_val = values[-1]
+
+    bin_tops = get_integer_histogram_bins(max_val, num_bins)
+
+
+    curr_index = 0
     
-    dmg_list = []
-    for _ in range(num_trials):
-        dmg =  final_damage(weap2_base, st + weap2_st_bonus,
-                            wp_skill, ft_skill,
-                            weap2_slay, enemy_ac,
-                            weap2_brand)
-        dmg_list.append(dmg)
+    for i in range(num_bins):
+        if i == 0:
+            bin_min = 0
+        else:
+            bin_min = bin_tops[i-1] + 1
+
+        bin_max = bin_tops[i]
+
+        num_in_bin = 0
         
+        if i == num_bins-1:
+            num_in_bin = (num_vals) - curr_index
+        else:
+            # Find the beginning and end of the range
+            bin_start_index = curr_index
+            while(values[curr_index] <= bin_max):
+                curr_index += 1
 
-    dmg2 = statistics.mean(dmg_list)
+            num_in_bin = curr_index - bin_start_index
 
-    print("Weap 1: " + str(dmg1))
-    print("Weap 2: " + str(dmg2))
+        percent_str = str(round(100*num_in_bin/num_vals, 2)) + "%"
+        print("{:2d} - {:2d}: ".format(bin_min, bin_max) + percent_str)
+
+
+# Returns a list with num_bins values, representing the high end of bins for
+# a histogram. The first value is always 0, and the last value is always
+# max_val. num_bins must be at least 2.
+#
+# When max_val doesn't divide evenly, the remainder increases the size of
+# the appropriate number of bins at the top end of the range by 1
+def get_integer_histogram_bins(max_val, num_bins):
+
+    # The even spacing gets split across (num_bins - 1) bins
+    spaced_bins = num_bins - 1
+    small_bin_size = max_val // spaced_bins
+
+    # The remainder is the number of bins that need to be large
+    # So the number of bins that need to be small is the number
+    # of bins with even spacing minus that
+    small_bins = spaced_bins - max_val % (num_bins - 1)
+    
+    bin_tops = [0]
+
+    for i in range(num_bins - 1):
+        size = small_bin_size
+
+        # if we need 3 small bins, then those are i = 0, 1, 2
+        # Anything larger is a big bin
+        if i >= small_bins:
+            size += 1
+
+        bin_tops.append(bin_tops[-1] + size)
+
+    return bin_tops
+        
+    
+    
 
  
 # Weapon damage after accounting for AC and brands
@@ -83,7 +172,9 @@ def damage_roll(base, st, wp_skill, ft_skill, slay):
     slay_bonus = roll(1+slay)
 
     # Apply modifiers
-    dmg = dmg * wp_mod * ft_mod + slay_bonus
+    dmg = int(dmg * wp_mod)
+    dmg = int(dmg * ft_mod)
+    dmg = dmg + slay_bonus
 
     
     return dmg
@@ -109,6 +200,9 @@ def calc_brand_damage(damage_done, brand):
         return 1 + roll(damage_done * 15)//10
 
     elif brand == "antimagic":
+        # (8 * dam) / (hitdice + 1) = duration increase
+        # spell success chance of enemy is 4/(4 + duration)
+        # Duration appears to be in turns
         return 0
 
     elif brand == "vorpal":
